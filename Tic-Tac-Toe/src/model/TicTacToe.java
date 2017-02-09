d37e0ade0cdcc29e32900e9dfd9a18a78314b357
JordanM1123 <jordan_munk@hotmail.com>
2017-02-09 17:48:33 +0100

    hasWon

--- Tic-Tac-Toe/src/model/TicTacToe.java
+++ Tic-Tac-Toe/src/model/TicTacToe.java
@@ -1,6 +1,8 @@
 package model;
 
+import javafx.scene.control.Alert;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 
@@ -22,6 +24,29 @@ public class TicTacToe {
         squaresOccupied = 0;
     }
 
+    public String hasPlayerWon(){
+        if(squareEnums[0] == SquareEnum.CIRCLE && squareEnums[1] == SquareEnum.CIRCLE && squareEnums[2] == SquareEnum.CIRCLE ||
+                squareEnums[3] == SquareEnum.CIRCLE && squareEnums[4] == SquareEnum.CIRCLE && squareEnums[5] == SquareEnum.CIRCLE ||
+                squareEnums[6] == SquareEnum.CIRCLE && squareEnums[7] == SquareEnum.CIRCLE && squareEnums[8] == SquareEnum.CIRCLE ||
+                squareEnums[0] == SquareEnum.CIRCLE && squareEnums[3] == SquareEnum.CIRCLE && squareEnums[6] == SquareEnum.CIRCLE ||
+                squareEnums[1] == SquareEnum.CIRCLE && squareEnums[4] == SquareEnum.CIRCLE && squareEnums[7] == SquareEnum.CIRCLE ||
+                squareEnums[2] == SquareEnum.CIRCLE && squareEnums[5] == SquareEnum.CIRCLE && squareEnums[8] == SquareEnum.CIRCLE ||
+                squareEnums[0] == SquareEnum.CIRCLE && squareEnums[4] == SquareEnum.CIRCLE && squareEnums[8] == SquareEnum.CIRCLE ||
+                squareEnums[2] == SquareEnum.CIRCLE && squareEnums[4] == SquareEnum.CIRCLE && squareEnums[6] == SquareEnum.CIRCLE) {
+            return "AI";
+        } else if(squareEnums[0] == SquareEnum.CROSS && squareEnums[1] == SquareEnum.CROSS && squareEnums[2] == SquareEnum.CROSS ||
+                squareEnums[3] == SquareEnum.CROSS && squareEnums[4] == SquareEnum.CROSS && squareEnums[5] == SquareEnum.CROSS ||
+                squareEnums[6] == SquareEnum.CROSS && squareEnums[7] == SquareEnum.CROSS && squareEnums[8] == SquareEnum.CROSS ||
+                squareEnums[0] == SquareEnum.CROSS && squareEnums[3] == SquareEnum.CROSS && squareEnums[6] == SquareEnum.CROSS ||
+                squareEnums[1] == SquareEnum.CROSS && squareEnums[4] == SquareEnum.CROSS && squareEnums[7] == SquareEnum.CROSS ||
+                squareEnums[2] == SquareEnum.CROSS && squareEnums[5] == SquareEnum.CROSS && squareEnums[8] == SquareEnum.CROSS ||
+                squareEnums[0] == SquareEnum.CROSS && squareEnums[4] == SquareEnum.CROSS && squareEnums[8] == SquareEnum.CROSS ||
+                squareEnums[2] == SquareEnum.CROSS && squareEnums[4] == SquareEnum.CROSS && squareEnums[6] == SquareEnum.CROSS) {
+            return "Player";
+        }
+        return "";
+    }
+
     public void squareClicked(Rectangle rectangle, int row, int column) throws IOException {
         int index = row * 3 + column;
         if (squareEnums[index] == SquareEnum.EMPTY) {
@@ -30,13 +55,59 @@ public class TicTacToe {
 
                 rectangle.setFill(new ImagePattern(img));
                 turn = SquareEnum.CIRCLE;
+                squareEnums[index] = SquareEnum.CROSS;
+                squaresOccupied += 1;
             } else if (turn == SquareEnum.CIRCLE) {
                 Image img = new Image(getClass().getResource("/resources/images/circle.png").toString());
                 rectangle.setFill(new ImagePattern(img));
                 turn = SquareEnum.CROSS;
+                squareEnums[index] = SquareEnum.CIRCLE;
+                squaresOccupied += 1;
             }
         }
 
         System.out.println("clicked "  + index);
+
+        if(hasPlayerWon().equals("AI")) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Information Dialog");
+            alert.setHeaderText(null);
+            alert.setContentText("AI (circle) has won the game!");
+
+            alert.showAndWait();
+            for (int i = 0; i < 9; i++) {
+                squareEnums[i] = SquareEnum.EMPTY;
+                //Ff functie voor alle plaatjes legen
+            }
+            squaresOccupied = 0;
+        }
+        if(hasPlayerWon().equals("Player")) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Information Dialog");
+            alert.setHeaderText(null);
+            alert.setContentText("Player (cross) has won the game!");
+
+            alert.showAndWait();
+
+            for (int i = 0; i < 9; i++) {
+                squareEnums[i] = SquareEnum.EMPTY;
+                //Ff functie voor alle plaatjes legen
+            }
+            squaresOccupied = 0;
+        }
+        if(squaresOccupied == 9) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Information Dialog");
+            alert.setHeaderText(null);
+            alert.setContentText("Game ended as a draw!");
+
+            alert.showAndWait();
+
+            for (int i = 0; i < 9; i++) {
+                squareEnums[i] = SquareEnum.EMPTY;
+                //Ff functie voor alle plaatjes legen
+            }
+        }
+
     }
 }
